f2f7175f5f84763a0827f8ce8c023882dbcecf5f
Alex <alexcoh19@outlook.com>
2025-10-22 14:35:54 +0200

    First commit. Added Enum.cs and Menu.cs. Copied Johannes' menu method.

---
+++ AFGRBank/Utility/Enum.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFGRBank.Utility
+{
+        // File to store all enums used in the project
+    public enum MainMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+    public enum LoginMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+    public enum UserMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+    public enum AdminMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+    public enum TransferMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+    public enum LoanMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+    public enum AccountMenuOptions
+    {
+        NewGame,
+        LoadGame,
+        Options,
+        Credits,
+        Exit,
+    }
+}
---
+++ AFGRBank/Utility/Menu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFGRBank.Utility
+{
+    internal class Menu
+    {
+        /// <summary>
+        /// Displays a menu with options for the user to choose from. They can navigate using arrow keys up and down. Clears console before displaying.
+        /// </summary>
+        /// <typeparam name="T">An array that is going to be displayed as strings</typeparam>
+        /// <typeparam name="TEnum">An enum that represents options in menu, where each index is an option</typeparam>
+        /// <param name="questionText">A string displayed at the top of the menu</param>
+        /// <param name="menuOptions">Options in an array that is displayed to the user</param>
+        /// <returns>Returns an Enum with chosen enum index</returns>
+        public static TEnum ReadOption<T, TEnum>(string questionText, T[] menuOptions) where TEnum : Enum
+        {
+            int i = 0;
+            while (true)
+            {
+                Console.Clear();
+                // Write out question and display options, currently selected index gets highlighted
+                Console.WriteLine(questionText + "\n");
+                for (int j = 0; j < menuOptions.Length; j++)
+                {
+                    Console.BackgroundColor = i == j ? ConsoleColor.White : ConsoleColor.Black;
+                    Console.ForegroundColor = i == j ? ConsoleColor.Black : ConsoleColor.White;
+                    Console.WriteLine(menuOptions[j]);
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                // Reset color and read the key that user presses
+                Console.ResetColor();
+                ConsoleKey key = Console.ReadKey().Key;
+                // Check what user pressed and go up or down in index as long as it is within the length of menuOptions. Enter returns Enum
+                switch (key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (i < menuOptions.Length - 1)
+                        {
+                            i++;
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (i > 0)
+                        {
+                            i--;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        // returns specified enum
+                        return (TEnum)Enum.ToObject(typeof(TEnum), i);
+
+                }
+            }
+        }
+    }
+}
