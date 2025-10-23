bf5591c29efc78a3004fdebb70d8d40a904ae7f6
Alex <alexcoh19@outlook.com>
2025-10-23 17:41:37 +0200

    Created Validation.cs for methods used to validate user inputs. BankingMain.cs created CreateUserMenu() for admins to create new users.

--- AFGRBank/UserType/Admin.cs
+++ AFGRBank/UserType/Admin.cs
@@ -14,7 +14,7 @@ namespace AFGRBank.UserType
         // Calls to create a new user
         // Loops through to make sure there are no duplicate UserNames
         // If there aren't, returns a new userlist with the created user
-        public List<User> CreateUser(string username, string password, string name, string surName, int phoneNumber, string address, List<User> userList)
+        public List<User> CreateUser(string username, string password, string name, string surName, string email, int phoneNumber, string address, List<User> userList)
         {
             List<User> newUserList = userList;
 
@@ -26,6 +26,7 @@ namespace AFGRBank.UserType
                     newUser.UserName = username;
                     newUser.Password = password;
                     newUser.Name = name;
+                    newUser.Email = email;
                     newUser.PhoneNumber = phoneNumber;
                     newUser.Address = address;
                     newUserList.Add(newUser);
