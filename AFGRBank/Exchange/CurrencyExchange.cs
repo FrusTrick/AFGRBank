862416943517ba93f596e17ef3a415dbc72e4c49
gituser4179 <mrito.fadi@hotmail.com>
2025-10-28 09:59:54 +0100

    Add CurrencyExchange class and CurrencyRates.json
    Introduced a new C# class `CurrencyExchange` in the `AFGRBank.CurrencyHandler` namespace. This class includes an enumeration `CurrencyName` with values SEK, DKK, EUR, USD, and YEN, properties for `Currency` and `Rate`, a constructor, and a method `GetExchangeRate` returning a fixed value.
    
    Added `CurrencyRates.json` containing currency exchange rates for SEK, EUR, and USD.

---
+++ AFGRBank/Exchange/CurrencyExchange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFGRBank.CurrencyHandler
+{
+    public class CurrencyExchange
+    {
+        public enum CurrencyName { SEK, DKK, EUR, USD, YEN }
+        public string Currency { get; set; }
+        public decimal Rate { get; set; }
+
+        public CurrencyExchange()
+        {   
+        }
+
+        public decimal GetExchangeRate(CurrencyName currency)
+        {
+            return 1;
+        }
+    }
+}
