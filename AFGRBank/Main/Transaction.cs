2973378e87fe39f3cc672578f71b3074e94bef06
rezan <nowrozireza1@gmail.com>
2025-11-05 11:07:00 +0100

    Created StartCountdown method for starting a 15 minute countdown. ConfirmTransaction method for confirming a transaction. CancelTransaction for cancelling a transaction. Initializing propertie sfor PendingTransaction

--- AFGRBank/Main/Transaction.cs
+++ AFGRBank/Main/Transaction.cs
@@ -21,8 +21,38 @@ namespace AFGRBank.Main
 
     }
 
-    public class PendingTransaction 
+    public class PendingTransaction : Transaction
     {
-        
+        public Transaction transaction { get; set; }
+        public bool Confirmed { get; set; } = false;
+        public User Sender { get; set; }
+        public User Receiver { get; set; }
+        public PendingTransaction()
+        {
+        }
+
+        public async Task StartCountdown(int minutes = 15)
+        {
+            await Task.Delay(TimeSpan.FromMinutes(minutes));
+            ConfirmTransaction();
+        }
+
+        public void ConfirmTransaction()
+        {
+
+            if (!Confirmed)
+            {
+                Confirmed = true;
+                Console.WriteLine("Transaction has been confirmed.");
+            }
+        }
+
+        public void CancelTransaction()
+        {
+            if (!Confirmed)
+            {
+                Console.WriteLine("Transaction has been cancelled due to timeout.");
+            }
+        }
     }
 }
